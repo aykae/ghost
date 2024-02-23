f9d7c428d29cf816b3dab413a6d14e801730d3f0
JoshWeissman <joshw11@seas.upenn.edu>
2024-02-23 13:15:17 -0500

    added policy page base

---
+++ frontend/src/pages/PolicyPage/index.tsx
@@ -0,0 +1,12 @@
+import { Header, Main } from '../../components';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <Header /> 
+      <Main />
+    </div>
+  )
+};
+
+export default HomePage;
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -3,13 +3,14 @@ import {
    HOME_PATH,
    POLICY_PATH,
 } from './paths';
-import { HomePage } from '../pages';
+import { HomePage, PolicyPage } from '../pages';
 
 const Router: React.FC = () => {
    return (
       <BrowserRouter>
          <Routes>
             <Route path={HOME_PATH} element={<HomePage />} />
+            <Route path={POLICY_PATH} element={<PolicyPage />} />
             <Route path="/" element={<Navigate replace to={HOME_PATH} />} />
          </Routes>
       </BrowserRouter>
