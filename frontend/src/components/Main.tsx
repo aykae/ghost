2e954d97d6a55a613040e6d76bc8ce8b7c0ab1cb
JoshWeissman <joshw11@seas.upenn.edu>
2024-02-23 16:41:12 -0500

    comments

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -12,9 +12,13 @@ const policy: Policy = {
 };
 
 export const Main: React.FC = () => {
+  // Add state for list of cards
+
   return (
     <Container sx={{ padding: '20px' }}> {/* Apply padding of 20px */}
       <Grid container spacing={2}>
+        {/* REMOVE MAPPING THING BELOW THAT MAPS PLACEHOLDER DATA */}
+        {/* ADD ACTUAL DATA FROM FETCHING POLICY CARD */}
         {[...Array(8)].map((_, index) => ( // Generate 8 PolicyCard components
           <Grid key={index} item xs={12} sm={6} md={4}>
             <PolicyCard {...policy} />
